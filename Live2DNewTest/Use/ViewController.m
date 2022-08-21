efe34997b36a3e77c86c130a944030ab07a0b91f
xiejiapei <xiejiapei@corp.netease.com>
2022-08-21 21:50:48 +0800

    [更新]

--- Live2DNewTest/Use/ViewController.m
+++ Live2DNewTest/Use/ViewController.m
@@ -18,32 +18,21 @@
     [super viewDidLoad];
 
     self.title = @"Live2D Demo";
-
     self.view.backgroundColor = UIColor.whiteColor;
-
+    
     UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-    [button setTitle:@"OpenGL" forState:UIControlStateNormal];
-    [button setTitleColor:UIColor.redColor forState:UIControlStateNormal];
-    [button addTarget:self action:@selector(pushOpenGLVc) forControlEvents:UIControlEventTouchUpInside];
-    button.frame = CGRectMake(100, 200, 80, 40);
-
-    [self.view addSubview:button];
-
-    button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.backgroundColor = UIColor.blackColor;
     [button setTitle:@"Metal" forState:UIControlStateNormal];
     [button setTitleColor:UIColor.redColor forState:UIControlStateNormal];
     [button addTarget:self action:@selector(pushMetalVc) forControlEvents:UIControlEventTouchUpInside];
-    button.frame = CGRectMake(100, 300, 80, 40);
+    button.frame = CGRectMake(150, 300, 150, 80);
 
     [self.view addSubview:button];
-}
-
-#pragma mark - event response
-- (void)pushOpenGLVc {
 }
 
 - (void)pushMetalVc {
     MetalLive2DViewController *vc = [MetalLive2DViewController new];
     [self.navigationController pushViewController:vc animated:true];
 }
+
 @end
